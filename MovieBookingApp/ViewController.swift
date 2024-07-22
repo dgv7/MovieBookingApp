992a1d735fff338d46e957f2e5bad5937678afca
upos <dgv7@daum.net>
2024-07-22 15:11:04 +0900

    Initial Commit

---
+++ MovieBookingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieBookingApp
+//
+//  Created by 김동건 on 7/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

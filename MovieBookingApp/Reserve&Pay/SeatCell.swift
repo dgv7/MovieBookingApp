853f63ddd458455ab31815e4f10c828926098065
ychic1109 <ychic1109@gmail.com>
2024-07-25 10:59:20 +0900

    no message

--- MovieBookingApp/Reserve&Pay/SeatCell.swift
+++ MovieBookingApp/Reserve&Pay/SeatCell.swift
@@ -1,23 +1,20 @@
 import UIKit
 import SnapKit
-
 class SeatCell: UICollectionViewCell {
-    let seatLabel = UILabel()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        contentView.addSubview(seatLabel)
-        seatLabel.textAlignment = .center
-        seatLabel.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
+  let seatLabel = UILabel()
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    contentView.addSubview(seatLabel)
+    seatLabel.textAlignment = .center
+    seatLabel.snp.makeConstraints { make in
+      make.edges.equalToSuperview()
     }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func configure(isSelected: Bool) {
-        contentView.backgroundColor = isSelected ? .green : .gray
-    }
+  }
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  func configure(isSelected: Bool, seatNumber: String) {
+    seatLabel.text = seatNumber
+    contentView.backgroundColor = isSelected ? .white : .gray
+  }
 }
